c1ababdc4dfe4dc47ea7cb9a5396387f0ea8f96c
Shea Lewis <shea.lewis89@gmail.com>
2015-03-17 17:02:58 -0700

    Various updates

--- src/Caffeinated/Github/Api/Repository/Contents.php
+++ src/Caffeinated/Github/Api/Repository/Contents.php
@@ -5,13 +5,46 @@ use Caffeinated\Github\Api\AbstractApi;
 
 class Contents extends AbstractApi
 {
-	public function archive($username, $repository, $format = 'tarball', $reference = null)
+	/**
+	 * Get archive for the given repository.
+	 *
+	 * @return mixed
+	 */
+	public function archive($owner, $repository, $format = 'tarball', $reference = null)
 	{
 		if (! in_array($format, ['tarball', 'zipball'])) {
 			$format = 'tarball';
 		}
 
-		return $this->get('repos/'.rawurlencode($username).'/'.rawurlencode($repository).'/'.rawurlencode($format).
+		return $this->get('repos/'.rawurlencode($owner).'/'.rawurlencode($repository).'/'.rawurlencode($format).
 			(! is_null($reference) ? ('/'.rawurlencode($reference)) : ''));
+	}
+
+	/**
+	 * Download a file from the given repository.
+	 *
+	 * @return null|string
+	 */
+	public function download($owner, $repository, $path, $reference = null)
+	{
+		$file = $this->show($owner, $repository, $path, $reference);
+
+		return base64_decode($file['content']) ?: null;
+	}
+
+	/**
+	 * Get contents of any file or directory in the given repository.
+	 *
+	 * @return array
+	 */
+	public function show($owner, $repository, $path = null, $reference = null)
+	{
+		$url = 'repos/'.rawurlencode($owner).'/'.rawurlencode($repository).'/contents';
+
+		if (! is_null($path)) {
+			$url .= '/'.rawurlencode($path);
+		}
+
+		return $this->get($url, ['ref' => $reference]);
 	}
 }
