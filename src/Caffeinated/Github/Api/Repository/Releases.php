11a0edeebaed3407c748194a2fbdeb0c6d6bc502
kaidesu <shea.lewis89@gmail.com>
2015-06-05 17:23:55 -0700

    Release API additions

--- src/Caffeinated/Github/Api/Repository/Releases.php
+++ src/Caffeinated/Github/Api/Repository/Releases.php
@@ -6,7 +6,10 @@ use Caffeinated\Github\Api\AbstractApi;
 class Releases extends AbstractApi
 {
 	/**
-	 * Returns an array of all repository releases.
+	 * List releases for a repository.
+	 *
+	 * Information about published releases are available to everyone. Only
+	 * users with push access will receive listings for draft releases.
 	 *
 	 * @param  string  $owner
 	 * @param  string  $repository
@@ -15,5 +18,48 @@ class Releases extends AbstractApi
 	public function all($owner, $repository)
 	{
 		return $this->get('repos/'.rawurlencode($owner).'/'.rawurlencode($repository).'/releases');
+	}
+
+	/**
+	 * Get a single release.
+	 *
+	 * @param  string  $owner
+	 * @param  string  $repository
+	 * @param  int     $id
+	 * @return array
+	 */
+	public function get($owner, $repository, $id)
+	{
+		return $this->get('repos/'.rawurlencode($owner).'/'.rawurlencode($repository).'/releases/'.rawurlencode($id));
+	}
+
+	/**
+	 * Get the latest release.
+	 *
+	 * View the latest published release for the repository.
+	 *
+	 * @param  string  $owner
+	 * @param  string  $repository
+	 * @return array
+	 */
+	public function latest($owner, $repository)
+	{
+		return $this->get('repos/'.rawurlencode($owner).'/'.rawurlencode($repository).'/releases/latest');
+	}
+
+	/**
+	 * Get a release by tag name.
+	 *
+	 * Get a release with the specified tag. Users must have push access to the
+	 * repository to view draft releases.
+	 *
+	 * @param  string  $owner
+	 * @param  string  $repository
+	 * @param  string  $tag
+	 * @return array
+	 */
+	public function tag($owner, $repository, $tag)
+	{
+		return $this->get('repos/'.rawurlencode($owner).'/'.rawurlencode($repository).'/releases/tags/'.rawurlencode($tag));
 	}
 }
